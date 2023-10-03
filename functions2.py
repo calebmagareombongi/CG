3757ec08885d9a3d5151183f28b641a1ea132941
calebmagareombongi <calebmagareombongi@gmail.com>
2023-10-03 16:43:38 +0300

    Caleb_Update functions2.py

--- functions2.py
+++ functions2.py
@@ -1,30 +1,50 @@
-import pandas as pd
 import os
 import json
+import pandas as pd
 
 # Function to split and save data
-def split_and_save_data(data, language, folder_path):
-    def split_and_save_data(data, language, folder_path):
-        # Specify proportions for the split
-        train_proportion = 0.70
-        test_proportion = 0.15
-        dev_proportion = 0.15
+def split_and_save_data(data, output_dir):
+    # Specify proportions for the split
+    train_proportion = 0.70
+    test_proportion = 0.15
+    dev_proportion = 0.15
 
-        # Calculate the split indices
-        total_samples = len(data)
-        train_split = int(train_proportion * total_samples)
-        test_split = train_split + int(test_proportion * total_samples)
+    # Calculate the split indices
+    total_samples = sum(len(language_data) for language_data in data.values())
+    train_split = int(train_proportion * total_samples)
+    test_split = train_split + int(test_proportion * total_samples)
 
-        # Split the data
-        train_data = data.iloc[:train_split]
-        test_data = data.iloc[train_split:test_split]
-        dev_data = data.iloc[test_split:]
+    # Initialize dictionaries to store split data
+    split_data = {
+        'train': [],
+        'test': [],
+        'dev': []
+    }
+
+    # Split the data
+    for language_code, language_data in data.items():
+        train_data = language_data[:train_split]
+        test_data = language_data[train_split:test_split]
+        dev_data = language_data[test_split:]
+
+        split_data['train'].extend(train_data)
+        split_data['test'].extend(test_data)
+        split_data['dev'].extend(dev_data)
 
         # Convert DataFrames to JSONL format and save with language-specific file names
-        train_data.to_json(os.path.join(folder_path, f'{language}_train.jsonl'), orient='records', lines=True)
-        test_data.to_json(os.path.join(folder_path, f'{language}_test.jsonl'), orient='records', lines=True)
-        dev_data.to_json(os.path.join(folder_path, f'{language}_dev.jsonl'), orient='records', lines=True)
+        train_df = pd.DataFrame(train_data)
+        test_df = pd.DataFrame(test_data)
+        dev_df = pd.DataFrame(dev_data)
 
+        train_df.to_json(os.path.join(output_dir, f'{language_code}_train.jsonl'), orient='records', lines=True)
+        test_df.to_json(os.path.join(output_dir, f'{language_code}_test.jsonl'), orient='records', lines=True)
+        dev_df.to_json(os.path.join(output_dir, f'{language_code}_dev.jsonl'), orient='records', lines=True)
+
+    # Save split data
+    for split_type, split_items in split_data.items():
+        with open(os.path.join(output_dir, f'{split_type}_split.jsonl'), 'w') as split_file:
+            for item in split_items:
+                split_file.write(json.dumps(item) + '\n')
 
 def main():
     # Ask the user for the folder path containing the Excel files
@@ -35,43 +55,36 @@ def main():
         print("The specified folder does not exist.")
         return
 
-    # Initialize dictionaries to store data for each language
-    data_by_language = {'en': pd.DataFrame(), 'sw': pd.DataFrame(), 'de': pd.DataFrame()}
+    # Initialize dictionary to store data for each language
+    data_by_language = {}
 
     # Iterate through Excel files in the folder
     for file in os.listdir(folder_path):
         if file.endswith(".xlsx"):
             file_path = os.path.join(folder_path, file)
 
-
             try:
                 # Determine the language from the file name
-                if 'en' in file:
-                    language = 'en'
-                elif 'sw' in file:
-                    language = 'sw'
-                elif 'de' in file:
-                    language = 'de'
-                else:
-                    # Skip files that don't contain language info
-                    continue
+                language = file.split('-')[1].split('.')[0]
+                if language not in data_by_language:
+                    data_by_language[language] = []
 
                 # Read data from each Excel file
                 data = pd.read_excel(file_path)
 
-                # Concatenate the data from each file into the main DataFrame
-                data_by_language[language] = pd.concat([data_by_language[language], data], ignore_index=True)
+                # Append data to the respective language list
+                data_by_language[language].append(data)
 
                 print(f"Data from '{file}' has been loaded.")
             except Exception as e:
                 print(f"An error occurred while loading data from '{file}': {e}")
 
     # Perform the split and save operation for each language
-    for language, data in data_by_language.items():
-        split_and_save_data(data, language, folder_path)
+    for language, language_data in data_by_language.items():
+        split_and_save_data(language_data, folder_path)
 
     # Generate the large JSON file showing translations from en to xx
-    large_json_data = {'en_to_xx': data_by_language['en'].to_dict(orient='records')}
+    large_json_data = {'en_to_xx': data_by_language['en'][0].to_dict(orient='records')}
 
     # Save the large JSON file
     large_json_file_path = os.path.join(folder_path, 'large_translation.json')
@@ -80,7 +93,6 @@ def main():
 
     print("Data has been split and saved to JSONL files.")
     print("The large JSON file has been generated.")
-
 
 if __name__ == "__main__":
     main()
