ddc0fa763dbb948f46179ed699e28d0c517bfe11
calebmagareombongi <calebmagareombongi@gmail.com>
2023-10-03 16:44:53 +0300

    Caleb_Update2 main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import functions
 import functions2
 
-# This function creates the flag and calls arguments.
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='Process the dataset and generate Excel files for different languages.')
@@ -28,14 +27,14 @@ def main():
 
     os.makedirs(output_dir, exist_ok=True)
 
-    # Load data from JSONL files
-    data = functions.load_data(input_dir)
+    # Load data from JSONL files (optimized for large files)
+    data = functions.load_data_optimized(input_dir)
 
     # Generate Excel files for all languages
     functions.generate_excel_files(data, output_dir, pivot_language='en')
 
-    # Split and save data
-    functions2.split_and_save_data(data, output_dir)
+    # Split and save data (optimized for parallel processing)
+    functions2.split_and_save_data_parallel(data, output_dir)
 
 if __name__ == "__main__":
     main()
